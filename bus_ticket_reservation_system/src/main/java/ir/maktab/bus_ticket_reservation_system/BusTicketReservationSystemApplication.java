e4189db1f477bad044e70bd39cb61b6149a436d2
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-10-23 16:49:25 +0330

    Add spring boot initialization for bus reservation System project

---
+++ bus_ticket_reservation_system/src/main/java/ir/maktab/bus_ticket_reservation_system/BusTicketReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package ir.maktab.bus_ticket_reservation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTicketReservationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusTicketReservationSystemApplication.class, args);
+    }
+
+}
---
+++ bus_ticket_reservation_system/src/test/java/ir/maktab/bus_ticket_reservation_system/BusTicketReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.maktab.bus_ticket_reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
