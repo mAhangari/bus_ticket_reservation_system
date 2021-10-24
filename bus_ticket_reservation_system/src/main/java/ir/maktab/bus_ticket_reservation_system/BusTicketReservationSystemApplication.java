f53a120030dd09c560b81cbe991c32c09ffc19a9
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-10-24 21:01:21 +0330

    Modify BusTicketReservationSystemApplication add @ServletComponentScan

--- bus_ticket_reservation_system/src/main/java/ir/maktab/bus_ticket_reservation_system/BusTicketReservationSystemApplication.java
+++ bus_ticket_reservation_system/src/main/java/ir/maktab/bus_ticket_reservation_system/BusTicketReservationSystemApplication.java
@@ -2,7 +2,9 @@ package ir.maktab.bus_ticket_reservation_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class BusTicketReservationSystemApplication {
 
